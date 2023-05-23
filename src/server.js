40d63d3a87158826dd18e0da89eba93f52fe0ed9
DoYun <wogns0624@naver.com>
2023-05-23 21:09:43 +0900

    socket.io를 이용한 백엔드와 프론트엔드연결

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import socketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,35 +10,44 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
+const httpServer = http.createServer(app);
+const wsServer = socketIO(httpServer);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconneted from the Browser ❌"));
-
-  // socket.on("message", (message) => {
-  //   sockets.forEach((aSocket) => aSocket.send(message));
-  // });  object blob이 떠서 아래코드로 바꿈`
-
-  socket.on("message", (msg) => {
-    // message = message.toString("utf-8");
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
   });
 });
 
-server.listen(3000, handleListen);
+// const wss = new WebSocket.Server({ server });
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anon";
+//   console.log("Connected to Browser ✅");
+//   socket.on("close", () => console.log("Disconneted from the Browser ❌"));
+
+//   // socket.on("message", (message) => {
+//   //   sockets.forEach((aSocket) => aSocket.send(message));
+//   // });  object blob이 떠서 아래코드로 바꿈`
+
+//   socket.on("message", (msg) => {
+//     // message = message.toString("utf-8");
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+// });
+const handleListen = () => console.log("Listening on http://localhost:3000");
+httpServer.listen(3000, handleListen);
