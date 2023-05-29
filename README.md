ec113125974bd179bce9f4e729bd4dbe4e7c1a1e
DoYun <wogns0624@naver.com>
2023-05-29 19:16:32 +0900

    채팅방에 입장,퇴장할때 알림과 인원수 구현

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# Noom
+# Zoom
 
 Zoom Clone using NodeJs, WebRTC and Websockets.
+
+on 메소드
+: 현재 접속되어 있는 클라이언트로부터 메시지를 수신하려면 on 메소드 사용
+
+'connection' : socket.io의 기본 이벤트, 사용자가 웹사이트에 접속하면 자동으로 발생하는 이벤트
+socket.on
+해당 클라이언트에서 메세지를 보낸다.
+
+io.emit
+서버가 현재 접속해있는 모든 클라이언트에게 이벤트 전달
+
+socket.emit
+서버쪽에서 event를 발생시키는 함수
+서버에서 이벤트 발생시키면 클라이언트 페이지의 해당 이벤트 리스너에서 처리
+해당 소켓을 통해 클라이언트에게 메시지 전송
--- src/server.js
+++ src/server.js
@@ -13,13 +13,54 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = socketIO(httpServer);
 
-wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done();
-    }, 10000);
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
   });
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Anon";
+  socket.onAny((event) => {
+    console.log(`Socket event: ${event}`);
+  });
+  //누군가 들어왔을때
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); //메세지를 하나의 소켓에만 보냄
+    wsServer.sockets.emit("room_change", publicRooms()); //메세지를 모든소켓에 보냄
+  });
+  //누군가 나갈때
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+    );
+  });
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
+  }); //채팅방을 떠나면 방이 사리짐을 알림
+
+  //메세지를 보낼때
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", `${socket.nickname}:${msg}`);
+    done();
+  });
+  //
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 // const wss = new WebSocket.Server({ server });
